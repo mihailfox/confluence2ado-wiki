b6439187a6ab1f43c954b2f2615a718c5de0fea3
Mihai Fox <foxmihail83@gmail.com>
2023-01-22 19:16:08 +0200

    refactor

--- migrate_confluence.py
+++ migrate_confluence.py
@@ -1,14 +1,23 @@
-import json
 from atlassian import Confluence
+import json
+import os
 
-# Read credentials from .secrets.json file
-with open(".secrets.json", "r") as f:
-    creds = json.load(f)
+# Load secrets from JSON file
+with open(".secrets.json", "r") as secrets_file:
+    secrets = json.load(secrets_file)
 
-# Connect to Confluence using personal token
-confluence = Confluence(url=creds["url"], personal_token=creds["personal_token"])
+confluence_url = secrets["confluence_url"]
+azure_devops_url = secrets["azure_devops_url"]
+space_key = secrets["space_key"]
+project_name = secrets["project_name"]
+confluence_token = secrets["confluence_token"]
+azure_devops_token = secrets["azure_devops_token"]
 
-# Define a function to convert a Confluence page to markdown
+confluence = Confluence(
+    url=confluence_url,
+    username=confluence_token
+)
+
 def confluence_to_markdown(page_id):
     page = confluence.get_page_by_id(page_id)
     markdown = confluence.convert_content_to_markdown(page["body"]["storage"]["value"])
