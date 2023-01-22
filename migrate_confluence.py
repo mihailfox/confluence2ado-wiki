ac22cd250f07eef169e18755c68b987761a7709c
Mihai Fox <foxmihail83@gmail.com>
2023-01-22 19:04:10 +0200

    add import export logic

--- migrate_confluence.py
+++ migrate_confluence.py
@@ -1,6 +1,7 @@
 from atlassian import Confluence
 from atlassian import AzureDevOps
 import json
+import os
 
 # Load secrets from JSON file
 with open(".secrets.json", "r") as secrets_file:
@@ -26,3 +27,22 @@ azure_devops = AzureDevOps(
 def export_import_pages(parent_id):
     child_pages = confluence.get_children_of_page(parent_id)
     return child_pages
+
+def export_page(child_page):
+    export_content = confluence.get_page_as_markdown(child_page['id'])
+    with open(f"{child_page['title']}.md", "w") as md_file:
+        md_file.write(export_content)
+    return child_page["title"]
+
+def import_page(file_name):
+    with open(f"{file_name}.md", "r") as md_file:
+        md_content = md_file.read()
+    azure_devops.create_wiki_page(project_name, file_name, md_content)
+    os.remove(f"{file_name}.md")
+
+root_page = confluence.get_root_page(space_key)
+child_pages = export_import_pages(root_page["id"])
+
+for child_page in child_pages:
+    file_name = export_page(child_page)
+    #import_page(file_name)
